1b3c1c6b611c29684a06d0de31d31c139b21678e
Alexander Leones <aleones101@gmail.com>
2022-04-02 18:53:29 -0700

    analyze your guesses helpfulness working

--- src/does_contain.rs
+++ src/does_contain.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 
-fn does_contain(word: &mut String, letters: &[char]) -> bool {
+fn _does_contain(word: &mut String, letters: &[char]) -> bool {
     let letters_set: HashSet<&char> = HashSet::from_iter(letters);
     // let original_size = word.chars().count();
     // word.retain(|x| !letters_set.contains(&x));
--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,10 @@
 // https://stackoverflow.com/a/25298960
 // dictionary txt file can be found at https://github.com/dwyl/english-words
 
-use std::array;
-use std::collections::{HashMap,};
+
+use std::collections::HashMap;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{self, BufRead, BufReader};
 mod does_contain;
 /*
 How it will work:
@@ -30,19 +30,89 @@ with this input:
 add the int from every tuple to dict of all leters to get if the word ever appears in the word as green or yellow.
 */
 
-fn get_input() -> Vec<[(char, usize); 5]> {
-    vec![
-        [('s', 0), ('a', 0), ('l', 0), ('e', 1), ('t', 1)],
-        [('p', 1), ('e', 1), ('t', 1), ('t', 0), ('y', 0)],
-    ]
+fn main() {
+    let input = get_input();
+    let vectors = determine_letters(&input);
+    let mut potential_words = find_words_with_known_and_unknown(&vectors.0, &vectors.1);
+
+    potential_words.retain(|x| {
+        for (i, letter) in x.chars().enumerate() {
+            if let Some(j) = vectors.2[i].get(&letter) {
+                if j < &2 {
+                    return false;
+                }
+            } else {
+                for v in &vectors.2[i] {
+                    if v.1 == &2 {
+                        return false;
+                    }
+                }
+            }
+        }
+        true
+    });
+
+    println!("{:?}", potential_words);
 }
 
-fn determine_letters(touples: &Vec<[(char, usize); 5]>) -> (Vec<char>, Vec<char>, HashMap<char, usize>) {
+fn get_input() -> Vec<[(char, usize); 5]> {
+    let mut guesses = vec![];
+    'mainloop: loop {
+        println!("Enter a word. All lowers, no spaces. \n Enter last to finish.");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess);
+        guesses.push(guess);
+        if guesses.last().unwrap().trim() == "last" {
+            guesses.pop();
+            break 'mainloop;
+        }
+    }
+
+    let mut res: Vec<[(char, usize); 5]> = vec![];
+    let mut arr = [(' ', 0_usize); 5];
+
+    for word in guesses {
+        let mut tup = (' ', 0);
+        let mut j = 0;
+        for (i, character) in word.chars().enumerate() {
+            if i % 2 != 0 {
+                tup.1 = character.to_digit(10).unwrap() as usize;
+                print!("{}", tup.0);
+                arr[j] = tup;
+                j += 1;
+            } else {
+                tup.0 = character;
+            }
+        }
+        println!();
+        res.push(arr);
+    }
+    res
+
+    // vec![
+    //     [('s', 0), ('a', 1), ('l', 0), ('e', 0), ('t', 0)],
+    //     [('a', 1), ('u', 0), ('d', 0), ('i', 0), ('o', 0)],
+    //     [('g', 0), ('r', 2), ('a', 2), ('p', 0), ('y', 2)],
+    // ]
+}
+
+fn determine_letters(
+    touples: &[[(char, usize); 5]],
+) -> (Vec<char>, Vec<char>, [HashMap<char, usize>; 5]) {
     let mut known_letters = HashMap::new();
-    let mut letter_map:[HashMap<char, usize>; 5] = [HashMap::new(), HashMap::new(), HashMap::new(), HashMap::new(), HashMap::new(),];
-    
+    let mut letter_map: [HashMap<char, usize>; 5] = [
+        HashMap::new(),
+        HashMap::new(),
+        HashMap::new(),
+        HashMap::new(),
+        HashMap::new(),
+    ];
+
     for arr in touples {
-        for t in arr {
+        for (i, t) in arr.iter().enumerate() {
+            if *letter_map[i].entry(t.0).or_insert(t.1) < t.1 {
+                letter_map[i].insert(t.0, t.1);
+            }
             if *known_letters.entry(t.0).or_insert(t.1) < t.1 {
                 known_letters.insert(t.0, t.1);
             }
@@ -58,157 +128,8 @@ fn determine_letters(touples: &Vec<[(char, usize); 5]>) -> (Vec<char>, Vec<char>
             grey.push(*letters.0);
         }
     }
-    (green_or_yello, grey, known_letters) // temporary return
+    (green_or_yello, grey, letter_map)
 }
-
-fn main() {
-    // let mut scrambled_word: String;
-    // loop {
-    //     scrambled_word = String::new();
-    //     println!("Enter a scrambled word:");
-    //     let b1 = std::io::stdin().read_line(&mut scrambled_word);
-    //     match b1 {
-    //         Ok(_) => {
-    //             let mut is_a_letter = true;
-    //             for c in scrambled_word.trim_end().chars() {
-    //                 if !c.is_alphabetic() {
-    //                     println!("Invalid input, make sure the scrambled word is alphabetic.");
-    //                     is_a_letter = false;
-    //                     break;
-    //                 }
-    //             }
-    //             if is_a_letter {
-    //                 break;
-    //             }
-    //         }
-    //         Err(_) => println!("Invalid input, try again."),
-    //     };
-    // }
-    // println!(
-    //     "{:?}",
-    //     find_valid_words(
-    //         /*replace following string with scrambled word you wish to unscramble. */
-    //         //"iverdfdsdafefrgsfcrervrs"
-    //         scrambled_word.as_str().trim()
-    //     )
-    // );
-
-
-    // let mut possible_words =
-    //     find_words_with_known_and_unknown(&['p', 'e', 't'], &['s', 'a', 'l', 'y']);
-    // possible_words.retain(|x| {
-    //     !x.starts_with('p')
-    //         && x.chars().nth(1).unwrap() != 'e'
-    //         && x.chars().nth(2).unwrap() != 't'
-    //         && x.chars().nth(3).unwrap() != 'e'
-    //         && x.chars().nth(4).unwrap() != 't'
-    // });
-    // println!("{:?}", possible_words)
-    let input = get_input();
-    let vectors = determine_letters(&input);
-    let mut possible_words = find_words_with_known_and_unknown(&vectors.0, &vectors.1);
-    // possible_words.retain(|x| {
-    //     !x.starts_with('p')
-    //         && x.chars().nth(1).unwrap() != 'e'
-    //         && x.chars().nth(2).unwrap() != 't'
-    //         && x.chars().nth(3).unwrap() != 'e'
-    //         && x.chars().nth(4).unwrap() != 't'
-    // });
-
-    possible_words.retain(|x| {
-        for arr in &input {
-            let mut ix = 0;
-            for letter in arr {
-                if letter.1 == 1 {
-                    if x.chars().nth(ix).unwrap() == letter.0 {
-                        return false;
-                    }
-                } else if letter.1 == 2 {
-                    if x.chars().nth(ix).unwrap() != letter.0 {
-                        return false;
-                    }
-                }
-                ix+=1;
-            }
-        }
-        true
-    });
-    println!("{:?}", possible_words)
-
-}
-
-// fn find_valid_words(letters: &str) -> Vec<String> {
-//     let mut avail = vec![0_usize; 26];
-//     // store the frequency of each letter in the string.
-//     for c in letters.chars() {
-//         let index = c as usize - 'a' as usize;
-//         avail[index] += 1;
-//     }
-
-//     // create a vector to store the result.
-//     let mut result: Vec<String> = Vec::new();
-
-//     // open the dictionary file and create an input buffer.
-//     let filename = "src/words_alpha.txt";
-//     let file = File::open(filename).unwrap();
-//     let reader = BufReader::new(file);
-
-//     // for each word in the dictionary..
-//     for word in reader.lines() {
-//         let mut count = vec![0; 26];
-//         let mut ok = true;
-//         let word = word.unwrap();
-//         /*
-//          * count the frequency of each character in the dictionary word.
-//          * set ok to false and break if too many of
-//          * a letter is found in the dictionary word.
-//          */
-//         for c in word.chars() {
-//             let index = c as usize - 'a' as usize;
-//             count[index] += 1;
-//             if count[index] > avail[index] {
-//                 ok = false;
-//                 break;
-//             }
-//         }
-//         // if word has the right amount of each letter, add to result vector.
-//         if ok && (word.len() == letters.len()) {
-//             result.push(word);
-//         }
-//     }
-//     result
-// }
-
-// fn find_words_with_letter(letter: char, in_position: usize) -> Vec<String>  {
-
-//     // create a vector to store the result.
-//     let mut result: Vec<String> = Vec::new();
-
-//     // open the dictionary file and create an input buffer.
-//     let filename = "src/words_alpha.txt";
-//     let file = File::open(filename).unwrap();
-//     let reader = BufReader::new(file);
-
-//     // for each word in the dictionary..
-//     for word in reader.lines() {
-
-//         let word = word.unwrap();
-//         /*
-//          * count the frequency of each character in the dictionary word.
-//          * set ok to false and break if too many of
-//          * a letter is found in the dictionary word.
-//          */
-//         let mut letter_at_4th_ix= ' ';
-
-//         if word.chars().count() == 5 {
-//             letter_at_4th_ix = word.chars().nth(in_position).unwrap();
-//             if letter == letter_at_4th_ix {
-//                 result.push(word);
-//             }
-//         }
-//     }
-//     result
-// }
 
 fn find_words_with_known_and_unknown(letters_in: &[char], letters_out: &[char]) -> Vec<String> {
     // create a vector to store the result.
